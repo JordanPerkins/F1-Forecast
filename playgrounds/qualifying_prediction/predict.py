1076f63b231a58b6b1eb1da3c3e57daf5e620fc3
Jordan Perkins <jordan@perkins.pw>
2020-02-14 19:36:11 +0000

    Model improvements

--- playgrounds/qualifying_prediction/predict.py
+++ playgrounds/qualifying_prediction/predict.py
@@ -108,6 +108,31 @@ def get_years_by_race():
 
 years_by_race = get_years_by_race()
 
+def get_qualifying_results_as_differences():
+    result = {}
+    for race, results in qualifying.items():
+        lap_times = list(results.values())
+        if len(lap_times):
+            fastest_lap = min(lap_times)
+            if race not in result:
+                result[race] = {}
+            for driver, qualifying_result in results.items():
+                result[race][driver] = round((qualifying_result - fastest_lap),3)
+    return result
+
+
+qualifying_as_differences = get_qualifying_results_as_differences()
+
+def get_previous_qualifying_result_as_differences(race, driver):
+    if race in qualifying_as_differences and driver in qualifying_as_differences[race]:
+        other_races = races_by_name[race_list[race]]
+        previous_race_index = other_races.index(race) - 1
+        if previous_race_index >= 0:
+            previous_race_id = other_races[previous_race_index]
+            if previous_race_id in qualifying_as_differences and driver in qualifying_as_differences[previous_race_id]:
+                return qualifying_as_differences[previous_race_id][driver]
+    return None
+
 
 def get_average_change(race, driver):
     total = 0
@@ -127,27 +152,77 @@ def get_average_change(race, driver):
             return round(total/count, 3)
     return None
 
+def get_average_change_by_differences(race, driver):
+    total = 0
+    count = 0
+    if race in years_by_race:
+        year = years_by_race[race]
+        races_in_year = races_by_year[year]
+        for season_race in races_in_year:
+            if season_race in qualifying_as_differences and driver in qualifying_as_differences[season_race]:
+                qualifying_lap = qualifying_as_differences[season_race][driver]
+                previous_qualifying_lap = get_previous_qualifying_result_as_differences(season_race, driver)
+                if previous_qualifying_lap:
+                    diff = qualifying_lap - previous_qualifying_lap
+                    total += diff
+                    count += 1
+        if count > 0:
+            return round(total/count, 3)
+    return None
+
+def get_season_change(race):
+    total = 0
+    count = 0
+    if race in years_by_race:
+        year = years_by_race[race]
+        races_in_year = races_by_year[year]
+        for season_race in races_in_year:
+            if season_race in qualifying:
+                for driver, lap in qualifying[season_race].items():
+                    previous_qualifying_lap = get_previous_qualifying_result(season_race, driver)
+                    if previous_qualifying_lap:
+                        diff = lap - previous_qualifying_lap
+                        total += diff
+                        count += 1
+        if count > 0:
+            return round(total/count, 3)
+    return None
+
+
+def get_fastest_qualifying_lap():
+    result = {}
+    for race, results in qualifying.items():
+        lap_times = list(results.values())
+        if len(lap_times):
+            fastest_lap = min(lap_times)
+            result[race] = fastest_lap
+    return result
+
+fastest_laps = get_fastest_qualifying_lap()
 
 def get_results(races):
     results = {
         'result': [],
         'race': [],
         'change': [],
-        'lap': []
+        'fastest_lap': [],
+        'lap': [],
+        'season_change': []
     }
     for race in races:
         if race in positions and race != "748":
             race_results = positions[race]
             for driver, position in race_results.items():
-                previous_result = get_previous_qualifying_result(race, driver)
-                average_change = get_average_change(race, driver)
-                if previous_result and average_change:
-                    diff = previous_result - qualifying[race][driver]
-                    if diff > -3:
-                        results['lap'].append(previous_result)
-                        results['result'].append(qualifying[race][driver])
-                        results['race'].append(race_list[race])
-                        results['change'].append(average_change)
+                previous_result = get_previous_qualifying_result_as_differences(race, driver)
+                average_change = get_average_change_by_differences(race, driver)
+                season_change = get_season_change(race)
+                if previous_result and average_change and season_change:
+                    results['fastest_lap'].append(fastest_laps[race])
+                    results['lap'].append(previous_result)
+                    results['result'].append(qualifying[race][driver])
+                    results['race'].append(race_list[race])
+                    results['change'].append(average_change)
+                    results['season_change'].append(season_change)
     for key in results.keys():
         results[key] = np.array(results[key])
     return results, results['result']
@@ -178,6 +253,8 @@ race_feature = tf.feature_column.categorical_column_with_vocabulary_list(
 feature_columns = [
     tf.feature_column.numeric_column(key='change'),
     tf.feature_column.numeric_column(key='lap'),
+    tf.feature_column.numeric_column(key='fastest_lap'),
+    tf.feature_column.numeric_column(key='season_change'),
     tf.feature_column.indicator_column(race_feature)
 ]
 
@@ -190,7 +267,7 @@ model = tf.estimator.DNNRegressor(
         l1_regularization_strength=0.001
     ))
 
-''''with open('training-log.csv', 'w') as stream:
+with open('training-log.csv', 'w') as stream:
     csvwriter = csv.writer(stream)
 
     for i in range(0, 200):
@@ -201,13 +278,15 @@ model = tf.estimator.DNNRegressor(
 
         print(evaluation_result)
 
-        csvwriter.writerow([(i + 1) * 100, evaluation_result['loss'], evaluation_result['average_loss']])'''
+        csvwriter.writerow([(i + 1) * 100, evaluation_result['loss'], evaluation_result['average_loss']])
 
 
 test_features1 = {
-    'race': np.array(['british']),
-    'lap': np.array([85.990]),
-    'change': np.array([-0.234])
+    'race': np.array(['british', 'british', 'british','british', 'british', 'british','british', 'british']),
+    'lap': np.array([0.325, 0.000,1.987,0.710,2.461,0.044, 1.207, 2.009]),
+    'change': np.array([-1.113, -2.855, -1.738, -2.362, -1.641, -2.3, -1.556, -0.905]),
+    'fastest_lap': np.array([85.892,85.892,85.892,85.892,85.892,85.892,85.892,85.892]),
+    'season_change': np.array([-1.817, -1.817, -1.817, -1.817, -1.817, -1.817, -1.817, -1.817])
 }
 
 test_input_fn1 = tf.estimator.inputs.numpy_input_fn(
@@ -219,7 +298,24 @@ test_input_fn1 = tf.estimator.inputs.numpy_input_fn(
 
 predictions = model.predict(input_fn=test_input_fn1)
 
-print(list(predictions))
+predictions_list = list(predictions)
 
+results = []
+for i in range(0, len(predictions_list)):
+    results.append((i, float(predictions_list[i]['predictions'][0])))
+print(results)
+
+sorted_results = sorted(results, key=lambda tup: tup[1])
+
+fastest_lap = sorted_results[0][1]
+
+differences = []
+
+print(sorted_results)
+
+'''for item in sorted_results:
+    differences.append(item[1]-fastest_lap)
+
+print(differences)'''
 
 
