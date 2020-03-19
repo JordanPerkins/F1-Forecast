fbd2a0119b910f1a996e7581fcff1de0cd2b4cc1
jordan <joperkins1@sheffield.ac.uk>
2020-03-19 19:55:03 +0000

    Add the forecast intent

--- alexa-skill/code/build/index.js
+++ alexa-skill/code/build/index.js
@@ -4,6 +4,8 @@
 const Alexa = require('ask-sdk-core');
 const { getRacePrediction, getQualifyingPrediction, searchForDriver } = require('./util.js');
 
+const FORECAST_DRIVERS_MAX = process.env.OVERRIDE_FORECAST_DRIVERS_MAX ? Number.parseInt(process.env.OVERRIDE_FORECAST_DRIVERS_MAX, 10) : 7
+
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
@@ -108,6 +110,121 @@ const PredictQualifyingPositionIntentHandler = {
             .getResponse();
     }
 };
+const ReadRaceForecastIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ReadRaceForecastIntent';
+    },
+    async handle(handlerInput) {
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        attributes.lastIntent = Alexa.getIntentName(handlerInput.requestEnvelope);
+        let speakOutput;
+        try {
+            const result = await getRacePrediction();
+            const data = result.data;
+            speakOutput = `Here is the race prediction forecast for the ${data.year} ${data.name} grand prix.`
+            for (let i = 0; i < FORECAST_DRIVERS_MAX; i++) {
+                if (i < data.result.length) {
+                    speakOutput += `<amazon:emotion name="excited" intensity="medium"><break time="1s"/><say-as interpret-as="ordinal">${i + 1}</say-as> ${data.result[i].driver_forename} ${data.result[i].driver_surname}</amazon:emotion>`
+                }
+            }
+            if (FORECAST_DRIVERS_MAX < data.result.length) {
+                attributes.lastPosition = FORECAST_DRIVERS_MAX;
+                attributes.lastResult = data;
+                return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('Would you like me to continue?')
+                .getResponse();
+            }
+            return handlerInput.responseBuilder
+            .speak(speakOutput);
+        } catch(e) {
+            console.error(`Error fetching result for ReadRaceForecastIntent: ${e}`);
+            speakOutput = 'I was unable to make a race prediction at this time. Please check back later'
+            return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+        }
+    }
+};
+const ReadQualifyingForecastIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ReadQualifyingForecastIntent';
+    },
+    async handle(handlerInput) {
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        attributes.lastIntent = Alexa.getIntentName(handlerInput.requestEnvelope);
+        let speakOutput;
+        try {
+            const result = await getQualifyingPrediction();
+            const data = result.data;
+            speakOutput = `Here is the qualifying prediction forecast for the ${data.year} ${data.name} grand prix.`
+            for (let i = 0; i < FORECAST_DRIVERS_MAX; i++) {
+                if (i < data.result.length) {
+                    speakOutput += `<amazon:emotion name="excited" intensity="medium"><break time="1s"/><say-as interpret-as="ordinal">${i + 1}</say-as> ${data.result[i].driver_forename} ${data.result[i].driver_surname}</amazon:emotion>`
+                }
+            }
+            if (FORECAST_DRIVERS_MAX < data.result.length) {
+                attributes.lastPosition = FORECAST_DRIVERS_MAX;
+                attributes.lastResult = data;
+                return handlerInput.responseBuilder
+                .speak(speakOutput)
+                .reprompt('Would you like me to continue?')
+                .getResponse();
+            }
+            return handlerInput.responseBuilder
+            .speak(speakOutput);
+        } catch(e) {
+            console.error(`Error fetching result for ReadQualifyingForecastIntent: ${e}`);
+            speakOutput = 'I was unable to make a qualifying prediction at this time. Please check back later'
+            return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+        }
+    }
+};
+const YesIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.YesIntent';
+    },
+    async handle(handlerInput) {
+        let speakOutput = '';
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        try {
+            if (attributes.lastIntent && (attributes.lastIntent === 'ReadRaceForecastIntent' || attributes.lastIntent === 'ReadQualifyingForecastIntent')) {
+                if (attributes.lastPosition && attributes.lastResult) {
+                    for (let i = attributes.lastPosition; i < attributes.lastPosition+FORECAST_DRIVERS_MAX; i++) {
+                        if (i < attributes.lastResult.result.length) {
+                            speakOutput += `<amazon:emotion name="excited" intensity="medium"><break time="1s"/><say-as interpret-as="ordinal">${i + 1}</say-as> ${attributes.lastResult.result[i].driver_forename} ${attributes.lastResult.result[i].driver_surname}</amazon:emotion>`
+                        }
+                    }
+                    if (attributes.lastPosition+FORECAST_DRIVERS_MAX < attributes.lastResult.result.length) {
+                        attributes.lastPosition = attributes.lastPosition+FORECAST_DRIVERS_MAX;
+                        attributes.lastResult = attributes.lastResult;
+                        attributes.lastIntent = attributes.lastIntent;
+                        return handlerInput.responseBuilder
+                        .speak(speakOutput)
+                        .reprompt('Would you like me to continue?')
+                        .getResponse();
+                    }
+                    speakOutput += '<break time="1s"/>Forecast concluded'
+                    return handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    .getResponse();
+                }
+            }
+        } catch (e) {
+            console.error(`Error fetching result for ReadRaceForecastIntent: ${e}`);
+            speakOutput = 'I was unable to carry out the requested action. Please check back later'
+            return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+        }
+    }
+};
+
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -130,6 +247,22 @@ const CancelAndStopIntentHandler = {
     },
     handle(handlerInput) {
         const speakOutput = 'Goodbye!';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+const NoIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.NoIntent');
+    },
+    handle(handlerInput) {
+        const attributes = handlerInput.attributesManager.getSessionAttributes();
+        attributes.lastPosition = undefined;
+        attributes.lastResult = undefined;
+        attributes.lastIntent = undefined;
+        const speakOutput = 'Ok. I will stop now. See you later!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .getResponse();
@@ -192,7 +325,11 @@ exports.handler = Alexa.SkillBuilders.custom()
         PredictQualifyingIntentHandler,
         PredictRacePositionIntentHandler,
         PredictQualifyingPositionIntentHandler,
+        ReadRaceForecastIntentHandler,
+        ReadQualifyingForecastIntentHandler,
         HelpIntentHandler,
+        YesIntentHandler,
+        NoIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler,
         IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
