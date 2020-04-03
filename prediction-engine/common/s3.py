4e6c236beb32da2d2d69036e8f53fd021ee1dc1a
jordan <joperkins1@sheffield.ac.uk>
2020-04-03 02:26:35 +0100

    Apply a fix for model fetching failure in backend

--- prediction-engine/common/s3.py
+++ prediction-engine/common/s3.py
@@ -12,11 +12,17 @@ BUCKET = os.getenv('S3_MODEL_BUCKET')
 s3 = boto3.client('s3')
 
 def fetch_model(model):
-    s3.download_file(BUCKET, model+'.tar.gz', FILE_DIR+model+'.tar.gz')
-    tarfile.open(FILE_DIR+model+'.tar.gz', 'r').extractall(FILE_DIR)
-    logging.info('Successfully retrieved ' + model + ' from S3')
-    return FILE_DIR+model
-
+    try:
+        s3.download_file(BUCKET, model+'.tar.gz', FILE_DIR+model+'.tar.gz')
+        tarfile.open(FILE_DIR+model+'.tar.gz', 'r').extractall(FILE_DIR)
+        logging.info('Successfully retrieved ' + model + ' from S3')
+        return FILE_DIR+model
+    except Exception as e:
+        logging.error('An error occured fetching the model: '+str(e))
+        if os.path.exists(FILE_DIR+model):
+            logging.info('An old model exists, so using that')
+            return FILE_DIR+model
+        raise
 
 def fetch_race_model():
     return fetch_model(RACE_MODEL)
